cbe6501626470c74b7f54208030f6cff9a123253
Jannik Himmelsbach <Jannik.Himmelsbach@gmx.de>
2024-06-01 10:51:01 +0100

    changed tour_size of tournament sel

--- selection.py
+++ selection.py
@@ -2,7 +2,7 @@ from operator import attrgetter
 from random import uniform, choice
 
 
-def tournament_sel(population, tour_size=7):
+def tournament_sel(population, tour_size=3):
     tournament = [choice(population) for _ in range(tour_size)]
     if population.optim == "max":
         return max(tournament, key=attrgetter('fitness'))
